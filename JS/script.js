cf615a39705ed2d21875fabd3073e35577ae21c3
alexandraquilao <alexandra.quilao@edu.sait.ca>
2024-11-29 20:32:37 -0700

    add numbered list js code

--- JS/script.js
+++ JS/script.js
@@ -40,3 +40,11 @@ AlertButton.addEventListener("mouseover"), function() {
 AlertButton.addEventListener("mouseout"), function() {
     this.innerText = "Click Me!";
 }; 
+
+// For loop to create numbered list
+const numbersList = document.getElementById("numbers");
+for (let i=1; i<=12; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = `List item ${i} is ${i % 2 === 0 ? 'even' : 'odd'}`;
+    numbersList.appendChild(listItem);
+}
