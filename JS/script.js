c7418edb18223dba2ddc3e030fc82cfa32246ab3
alexandraquilao <alexandra.quilao@edu.sait.ca>
2024-11-29 21:03:02 -0700

    delete ordered list js code

--- JS/script.js
+++ JS/script.js
@@ -49,10 +49,3 @@ AlertButton.addEventListener("mouseout", function() {  // remove extra parenthes
     this.innerText = "Click Me!";
 });
 
-// For loop to create numbered list
-const numbersList = document.getElementById("numbers");
-for (let i=1; i<=12; i++) {
-    const listItem = document.createElement("li");
-    listItem.textContent = `List item ${i} is ${i % 2 === 0 ? 'even' : 'odd'}`;
-    numbersList.appendChild(listItem);
-}
